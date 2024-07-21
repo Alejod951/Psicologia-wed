0bc4a50f9862ffbcb2eddb822a7113511ed51c38
Alejod951 <Alejod951@gmail.com>
2024-07-20 19:14:04 -0500

    Verce

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,18 @@
 import NavBar from "./componets/NavBar/NavBar.tsx";
-//import HomePage from "./componets/HomePage/HomePage.tsx";
+import HomePage from "./componets/HomePage/HomePage.tsx";
 import Tratamientos from "./componets/Tratamientos/Tratamientos.tsx"
 import { Routes, Route } from "react-router-dom";
 import './App.css';
 import AgendarCitas from "./componets/AgendarCitas/AgendarCitas.tsx";
+
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Routes>
-        <Route path="/" element={<Tratamientos />} />
-        <Route path="/tratamientos" element={<Tratamientos />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="https://valentina-eight.vercel.app/tratamientos" element={<Tratamientos />} />
         <Route path="/agendar" element={<AgendarCitas />} />
         <Route path="/contactanos" element={<AgendarCitas />} />
       </Routes>
